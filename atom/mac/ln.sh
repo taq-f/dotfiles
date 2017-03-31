c65aef011c1662a5b9c67068a6fd4ea0bc78cca7
taq-f <mysecondproject@gmail.com>
2017-03-31 21:11:12 +0900

    【Atom】mac script

---
+++ atom/mac/ln.sh
@@ -0,0 +1,2 @@
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+ln -s $SCRIPT_DIR/../keymap.cson ~/.atom
--- atom/windows/mklink.bat
+++ atom/windows/mklink.bat
@@ -4,8 +4,8 @@ del %USERPROFILE%\.atom\keymap.cson
 del %USERPROFILE%\.atom\snippets.cson
 del %USERPROFILE%\.atom\styles.less
 
-mklink %USERPROFILE%\.atom\config.cson %~p0\config.cson
-mklink %USERPROFILE%\.atom\init.coffee %~p0\init.coffee
-mklink %USERPROFILE%\.atom\keymap.cson %~p0\keymap.cson
-mklink %USERPROFILE%\.atom\snippets.cson %~p0\snippets.cson
-mklink %USERPROFILE%\.atom\styles.less %~p0\styles.less
+mklink %USERPROFILE%\.atom\config.cson %~p0\..\config.cson
+mklink %USERPROFILE%\.atom\init.coffee %~p0\..\init.coffee
+mklink %USERPROFILE%\.atom\keymap.cson %~p0\..\keymap.cson
+mklink %USERPROFILE%\.atom\snippets.cson %~p0\..\snippets.cson
+mklink %USERPROFILE%\.atom\styles.less %~p0\..\styles.less
